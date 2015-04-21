20ce25ad44815e7addfaf58b76bfe56f2f16cd5b
Mike Logsdon <michaelogsdon@gmail.com>
2015-04-21 09:25:02 -0700

    Now with a function to get weather

--- R/weatherHelper.R
+++ R/weatherHelper.R
@@ -1,35 +1,78 @@
 #Weather Data Functions...
+C_to_F <- function(x) x * 1.8 + 32
+key <- "mgPWFcAfmzIrHjXlacsksLXkhajnyFDp"
+
+read.noaa <- function(table, param = NULL) {
+  if(!is.null(param)) {
+    urlx <- paste("http://www.ncdc.noaa.gov/cdo-web/api/v2/", table, "?", param, sep = "")  
+  } else {
+    urlx <- paste("http://www.ncdc.noaa.gov/cdo-web/api/v2/", table, sep = "")  
+  }
+  print(paste("Attempting to query:", urlx))
+  tmp <- httr::GET(urlx, httr::add_headers(token = key))
+  jsonlite::fromJSON(httr::content(tmp, "text"))$results
+}
+
+# Here is a stationid... for testing...
+# stationid <- "GHCND:USW00024233"
+# startdate <- "2012-01-01"
+# enddate <- "2012-02-01"
 
 
-getGHCN <- function(ID) {
-  #Read in the country info
-  countriesURL <- getURL("ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-countries.txt")
-  ct <- readLines(textConnection(countriesURL))
-  countryCode <- gsub("^([A-Z]+) (.+)$", "\\1", ct)
-  countryName <- gsub("^([A-Z]+) (.+)$", "\\2", ct)
-  countryName <- rev(gsub("^ *(.+)$", "\\1", rev(countryName)))
-  countries <- data.frame("country" = countryName, "code" = countryCode)
-
-  #Read in the station info
-  stations <- getURL("ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt")
-  ts <- read.fwf(file = textConnection(stations),
-                 widths = c(11, -1, 8, -1, 9, -1, 6, -1, 2, -1, 30, -1, 3, -1, 3, -1, 5),fill = TRUE)
-  names(ts) <- c("id", "lat", "lon", "elev", "state", "name", "gsnFlag", "hcnFlag", "wmoid")
-  head(ts)
+read.one.ghcn <- function(stationid, startdate, enddate) {
+  # Set-up the parameters for the call to NOAA
+  startdate <- as.character(startdate)
+  enddate <- as.character(enddate)
+  param <- paste0("datasetid=GHCND&",
+                  "stationid=", stationid, "&",
+                  "startdate=", startdate, "&",
+                  "enddate=", enddate, "&",
+                  "datatypeid=TMIN,TMAX&",
+                  "limit=1000")
   
-  ts$elev[ts$elev == "-999.9"] <- NA
+  # Query the NOAA API for the data
+  dset <- read.noaa("data", param)
+  if(is.null(dset)) {
+    stop("NOAA Query did not return results")
+  }
   
-  st <- read.table(text = stations, header = FALSE, strip.white = TRUE, fill = TRUE)
-  
-
-  urlTmp <- "ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/"  
-  fnames <- getURL(urlTmp)
-  
-  #download.file("ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt", "stations.txt")
-  
-
-  
-  st$country <- gsub("^([A-Z]+)[0-9].+$", "\\1", st$V1)
+  # Clean it up and return
+  dset <- dset %>%
+    dplyr::mutate(date = as.Date(date)) %>%
+    dplyr::mutate(value = C_to_F(value / 10)) %>%
+    dplyr::select(date, value, datatype) %>%
+    tidyr::spread(datatype, value) %>%
+    dplyr::mutate(aveTemp = (TMIN + TMAX) / 2)
   
 }
 
+
+# startdate <- "2010-01-01"
+# enddate <- "2014-01-01"
+
+read.ghcn <- function(stationid, startdate, enddate) {
+  stDate <- try(as.Date(startdate))
+  if(inherits(stDate, "try-error")) {
+    stop(paste("Could not parse start date", startdate))
+  }
+  
+  edDate <- try(as.Date(enddate))
+  if(inherits(edDate, "try-error")) {
+    stop(paste("Could not parse end date", enddate))
+  }
+  
+  # We can download 1 year at a time
+  interval <- as.numeric(difftime(edDate, stDate))
+  nCalls <- ceiling(interval / 365)
+  
+  dsets <- lapply(1:nCalls, function(i) {
+    read.one.ghcn(stationid, stDate + (i - 1) * 365, min(edDate, stDate + i * 365 - 1))
+  })
+  
+  do.call('rbind', dsets)
+}
+
+# test <- read.one.ghcn("GHCND:USW00024233", "2013-12-31", "2014-12-31")
+# ggplot(test) + geom_line(aes(date, aveTemp))
+# test2 <- read.ghcn("GHCND:USW00024233", "2010-01-01", "2014-12-31")
+# ggplot(test2) + geom_line(aes(date, aveTemp))
