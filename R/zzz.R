57307bf43d89ed1ffca1b20f3e94cdca325a3075
Michael Logsdon <michaelogsdon@gmail.com>
2015-07-02 11:56:15 -0700

    Made an actually useful startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,15 @@
 
 .onAttach <- function(libname, pkgname) {
-  if(rbinom(1, 1, .1)) packageStartupMessage("https://twitter.com/kobebryant")
+  # if(rbinom(1, 1, .1)) packageStartupMessage("https://twitter.com/kobebryant")
   
   if(!exists("noaa_key")) {
     warning("NOAA Key for Downloading Weather Data Not Found. See help(read.noaa) for details.")
   }
+  
+  if(!exists("google_key")) {
+    warning("Google Maps API key not found. See help(stationSearch) for more info.")
+  }
+
+  packageStartupMessage("rterm development version, beware of bugs!")
+  
 }
